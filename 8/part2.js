178e1c5d735374a045e66bc78afb410cd24e5f4a
Denis Kurov <kurovda@gmail.com>
2024-12-08 10:04:24 +0300

    8

--- 8/part2.js
+++ 8/part2.js
@@ -20,7 +20,6 @@ const findSignals = (arr, antenas) => {
     const signalsUnique = new Set()
     const rows = arr.length
     const cols = arr[0].length
-    console.log({ rows, cols });
     for (const [key, value] of Object.entries(antenas)) {
         for (let i = 0; i < value.length; i++) {
             for (let j = i + 1; j < value.length; j++) {
@@ -32,20 +31,15 @@ const findSignals = (arr, antenas) => {
                 signalsUnique.add(JSON.stringify(pointA))
                 signalsUnique.add(JSON.stringify(pointB))
 
-
-                const linePoints = [pointA, pointB];
                 let nextPoint = { i: pointB.i + di, j: pointB.j + dj };
                 while (nextPoint.i >= 0 && nextPoint.i < rows && nextPoint.j >= 0 && nextPoint.j < cols) {
                     signalsUnique.add(JSON.stringify(nextPoint))
-                    linePoints.push(nextPoint);
-
                     nextPoint = { i: nextPoint.i + di, j: nextPoint.j + dj };
                 }
 
                 let prevPoint = { i: pointA.i - di, j: pointA.j - dj };
                 while (prevPoint.i >= 0 && prevPoint.i < rows && prevPoint.j >= 0 && prevPoint.j < cols) {
                     signalsUnique.add(JSON.stringify(prevPoint))
-                    linePoints.unshift(prevPoint);
                     prevPoint = { i: prevPoint.i - di, j: prevPoint.j - dj };
                 }
             }
